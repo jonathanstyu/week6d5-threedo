83099df5c74d25d0632cd43aea6903bde12fbff5
Jonathan Yu <jonathan@jonathanyu.net>
2013-06-21 14:52:58 -0700

    Add new nested item attributes

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,6 +1,7 @@
 class List < ActiveRecord::Base
-  attr_accessible :title, :item_ids 
+  attr_accessible :title, :item_ids, :items_attributes 
   
   has_many :items, :inverse_of => :list
   
+  accepts_nested_attributes_for :items
 end
