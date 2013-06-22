6aba0464aef5fd494a7a77cee934368351fe2b0a
Jonathan Yu <jonathan@jonathanyu.net>
2013-06-21 17:51:34 -0700

    delete lists kinda implemented

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,9 +1,9 @@
 class List < ActiveRecord::Base
   attr_accessible :title, :item_ids, :items_attributes 
   
-  has_many :items, :inverse_of => :list
+  has_many :items, :inverse_of => :list, :dependent => :destroy 
   
-  accepts_nested_attributes_for :items
+  accepts_nested_attributes_for :items, :reject_if => :all_blank
   
 
 end
